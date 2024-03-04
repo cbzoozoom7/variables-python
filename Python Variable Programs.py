1dd59b21abb1bf17a30d112fc49ad2bcc52d70e3
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-03-04 10:24:46 -0600

    Add files via upload

---
+++ Python Variable Programs.py
@@ -0,0 +1,43 @@
+#7: Math Trick 2
+one = input("Enter a number. ")
+print"Your answer is ", (((((one*3)+45)*2)/6)-one)
+
+#6: Math Trick 1
+one = input("Enter a number less than 10. ")
+print"Your answer is ", ((((one*2)+6)/2)-one), "."
+
+#5
+x = input("Enter an integer for x. ")
+y = input("Enter an integer for y. ")
+z = input("Enter a decimal for z. ")
+print""
+print"The product of x and y is ", (x * y), "."
+print"The product of x, y, and z is ", (x * y * z), "."
+print x, " mod 2 is ", (x % 2), "."
+print y, " mod 2 is ", (y % 2), "."
+print z, " mod 2 is ", (z % 2), "."
+
+#4
+year = input("What is the currrent year? ");
+name = raw_input("What is your name? ");
+age = input("What is your age? ");
+print""
+print"Your name is ", name, " and you're ", age, "years old."
+print"You were born in ", (year - age)
+
+#3
+numerator = input("What is the numerator? ");
+denominator = input("What is the denominator? ");
+print"The decimal value is ", (float(numerator) / denominator)
+
+#2
+one = input("What is the first number? ")
+two = input("What is the second number? ")
+three = input("What is the third number? ")
+four = input("What is the fourth number? ")
+print"The average is ", ((float(one) + two + three + four)/4)
+
+#1
+print("I'm Christopher Bowman")
+print("I play Factorio")
+print("I like Oreos")
